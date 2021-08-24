5888ad2f7198cfa6550b95add6e2988eb8b27a57
dbastosdev <dbastos.dev@gmail.com>
2021-08-24 07:49:45 -0300

    app finalizado

--- Bin2Dec/app/src/main/java/com/example/bin2dec/MainActivity.java
+++ Bin2Dec/app/src/main/java/com/example/bin2dec/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.bin2dec;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+    // Aqui entra um método para eventos do botão e conversão
+    public void convert (View view){
+        // Faz o mapeamento do campo de entrada
+        EditText bin = findViewById(R.id.binary_field);
+        // pega a entrada do usuário
+        String convert = bin.getEditableText().toString();
+        // Faz conversão para decimal
+        int finalConvert = Integer.parseInt(convert,2);
+        //Faz o mapeamento da saída e exibe resultado
+        TextView result = findViewById(R.id.result);
+        result.setText(String.valueOf(finalConvert));
+    }
+
 }
